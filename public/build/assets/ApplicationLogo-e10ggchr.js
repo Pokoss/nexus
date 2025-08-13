504894e920150b249bcd094a13c058d958d8963f
Pokoss <pokoss333@gmail.com>
2025-08-13 11:31:40 +0300

    image changes

--- public/build/assets/ApplicationLogo-e10ggchr.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-CKPQZwhe.js";function i(r){return o.jsx("div",{children:o.jsx("img",{...r,src:"/images/resources/logo.png"})})}export{i as A};
--- public/build/assets/RestrictedScreen-BKxG2ag9.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-CKPQZwhe.js";import t from"./Navbar-BnjWPU39.js";import"./index-BJug-v9R.js";function n(){return e.jsxs("div",{className:"",children:[e.jsx(t,{}),e.jsx("div",{className:"w-full flex justify-center mt-56 text-lg font-bold p-8 bg-blue-gray-500",children:"Access Denied Contact Admin"})]})}export{n as default};
